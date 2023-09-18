fbd9bd963d98b8c6bd3d943df89af6d8d9904c1e
‘Tarold’ <ltdecorer@gmail.com>
2023-09-18 11:54:51 +0300

    Add firemall movement and fix add child error

--- addons/FireBall.gd
+++ addons/FireBall.gd
@@ -1,13 +1,17 @@
 extends Area2D
 
 const explosionn = preload("res://addons/Explosion.tscn")
+const speed = 10
 
 func _ready():
 	pass
+
+func _physics_process(delta):
+	position += Vector2.RIGHT.rotated(rotation) * speed 
 	
 
 func _on_FireBall_body_entered(body):
 	var explosion = explosionn.instance()
 	explosion.position = position
-	get_parent().add_child(explosion)
+	get_parent().call_deferred("add_child", explosion)
 	queue_free()
